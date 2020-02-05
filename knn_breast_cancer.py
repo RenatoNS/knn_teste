33e001fd7e1d21fd869b99ea3eda5d10378f6fc8
Renato Nascimento da Silva <renatons@tce.rj.gov.br>
2020-02-05 15:34:03 -0300

    adicionando os imports

---
+++ knn_breast_cancer.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  5 15:28:59 2020
+
+@author: renatons
+"""
+#%%
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.datasets import load_breast_cancer
+from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+sns.set()
+from collections import Counter
+
+#%%
